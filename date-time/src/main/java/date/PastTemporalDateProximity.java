31cdf64da26dad8bfb3b5b8640146f39b6e56a79
AlexanderBaranov <avbc@mail.ru>
2024-08-13 11:17:29 +0300

    HW

--- date-time/src/main/java/date/PastTemporalDateProximity.java
+++ date-time/src/main/java/date/PastTemporalDateProximity.java
@@ -1,16 +1,45 @@
 package date;
 
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
+import java.util.Arrays;
 
-public class PastTemporalDateProximity implements TemporalAdjuster{
-//TODO some encapsulation
-//array of temporals supprting Day, Month, Year (Dates) 
+public class PastTemporalDateProximity implements TemporalAdjuster {
+    Temporal[] temporals;
+
+    public PastTemporalDateProximity(Temporal[] temporals) {
+        Temporal[] resultTemporals = java.util.Arrays.copyOf(temporals, temporals.length);
+        java.util.Arrays.sort(resultTemporals);
+        this.temporals = resultTemporals;
+    }    
+
     @Override
     public Temporal adjustInto(Temporal temporal) {
-        // TODO Auto-generated method stub
-        //return the temporal for the encapsulated array that is a nearest in past
-        throw new UnsupportedOperationException("Unimplemented method 'adjustInto'");
+        Temporal result = null;
+        int index = getIndexOfPastTemporalDateProximity(temporal);
+        if ( index >= 0 && index < temporals.length) {
+            result = temporals[index];
+            long days = ChronoUnit.DAYS.between(temporal, result);
+            result = temporal.plus(days, ChronoUnit.DAYS);
+        }
+        return result;
+    }
+
+    private int getIndexOfPastTemporalDateProximity(Temporal temporal) {
+        int start = 0;
+        int finish = temporals.length - 1;
+        int middle = start + (finish - start) / 2;
+        while (start <= finish) {
+            if (temporal.until(temporals[middle], ChronoUnit.DAYS) >= 0) {
+                finish = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+            middle = start + (finish - start) / 2;
+        }
+        return start - 1;
     }
 
 }
--- date-time/src/test/java/date/DateTimeTest.java
+++ date-time/src/test/java/date/DateTimeTest.java
@@ -1,10 +1,17 @@
 package date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
-import java.time.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+import java.time.chrono.JapaneseDate;
+import java.time.chrono.MinguoDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.util.Locale;
 
@@ -12,26 +19,52 @@ import org.junit.jupiter.api.Test;
 
 public class DateTimeTest {
     @Test
-    void localDateTest() {
-        LocalDate current = LocalDate.now();
-        LocalDateTime currentTime = LocalDateTime.now();
-        ZonedDateTime currentZonedTime = ZonedDateTime.now();
+    void LocalDateTest() {
+        LocalDate currentLocalDate = LocalDate.now();
+        LocalDateTime currentLocalDateTime = LocalDateTime.now();
+        ZonedDateTime currentZonedDateTime = ZonedDateTime.now();
         Instant currentInstant = Instant.now();
         LocalTime currentLocalTime = LocalTime.now();
-        System.out.printf("Current date is %s in ISO format \n",current);
-        System.out.printf("Current date & time is %s in ISO format \n",currentTime);
-        System.out.printf("Current zoned date & time is %s in ISO format \n",currentZonedTime);
-        System.out.printf("Current instantr %s in ISO format \n",currentInstant);
-        System.out.printf("Current time  %s in ISO format \n",currentLocalTime);
-        System.out.printf("Current date is %s in dd/mm/yyyy \n",current.format(DateTimeFormatter.ofPattern("dd/MMMM/yyyy", Locale.forLanguageTag("he"))));
+        System.out.printf("Current local date is %s in ISO format\n", currentLocalDate);
+        System.out.printf("Current local time is %s in ISO format\n", currentLocalTime);
+        System.out.printf("Current local date and time is %s in ISO format\n", currentLocalDateTime);
+        System.out.printf("Current zoned date and time is %s in ISO format\n", currentZonedDateTime);
+        System.out.printf("Current instant is %s in ISO format\n", currentInstant);
+        System.out.printf("Current local date is %s in dd/month/yyyy\n",
+                currentLocalDate.format(DateTimeFormatter.ofPattern("dd/MMMM/yyyy", Locale.forLanguageTag("en"))));
     }
+
     @Test
     void nextFriday13Test() {
-        LocalDate current = LocalDate.of(2024,8,11);
+        LocalDate current = LocalDate.of(2024, 8, 11);
         LocalDate expected = LocalDate.of(2024, 9, 13);
         TemporalAdjuster adjuster = new NextFriday13();
-        assertEquals(expected, current.with(adjuster));
+        assertEquals(expected, current.with(new NextFriday13()));
         assertThrows(RuntimeException.class, () -> LocalTime.now().with(adjuster));
-
     }
+
+      @Test
+    void pastTemporalDateProximityTest() {
+        LocalDate date10Feb = LocalDate.of(2024, 2, 10);
+        LocalDate date15Mar = LocalDate.of(2024, 3, 15);
+        LocalDate date20Apr = LocalDate.of(2024, 4, 20);
+        LocalDate date25May = LocalDate.of(2024, 5, 25);
+        LocalDate date30Jun = LocalDate.of(2024, 6, 30);
+
+        PastTemporalDateProximity adjuster = new PastTemporalDateProximity(new Temporal[]{
+            date20Apr, date25May, date10Feb
+        });
+
+        assertNull(date10Feb.with(adjuster));
+        assertEquals(date10Feb, date15Mar.with(adjuster));
+        assertEquals(date10Feb, date20Apr.with(adjuster));
+        assertEquals(date20Apr, date25May.with(adjuster));
+        assertEquals(date25May, date30Jun.with(adjuster));
+
+        assertEquals(MinguoDate.from(date10Feb), MinguoDate.from(date20Apr).with(adjuster));
+        assertEquals(JapaneseDate.from(date10Feb), JapaneseDate.from(date20Apr).with(adjuster));
+
+        assertThrows(RuntimeException.class, () -> LocalTime.now().with(adjuster));
+    }
+
 }
